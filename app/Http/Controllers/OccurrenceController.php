77b4a4e3c4c3c2eae2b1aa05a4079e49174bc176
majoch abdessamad <unleopard@gmail.com>
2022-05-25 15:15:18 +0100

    first route

--- app/Http/Controllers/OccurrenceController.php
+++ app/Http/Controllers/OccurrenceController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class OccurrenceController extends Controller
 {
-    //
+    public function index(Request $request) {
+        dd('first rout');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd('first route');
+});
+
+
+
+Route::get('/', function () {
     return view('welcome');
 });
